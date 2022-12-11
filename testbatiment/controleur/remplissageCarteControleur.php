f0bf9dec349d3c958d0514296b09350c6cb6a281
SebastienHus <hus.sebastien@gmail.com>
2022-12-11 13:58:13 +0100

    epurage du code des lignes inutiles

--- testbatiment/controleur/remplissageCarteControleur.php
+++ testbatiment/controleur/remplissageCarteControleur.php
@@ -35,20 +35,15 @@
             sinon  log d'erreur
     */
 
-    //Etape 0 
-    //$nbEmptyBox = rand(1,4);deporter dans la fonction d insertion du joueur
-
     //Etape 1 
     $xLimitWorldMap = WorldMap::getInfoWorldMap(X_MAX_MAP);
     $yLimitWorldMap = WorldMap::getInfoWorldMap(Y_MAX_MAP);
-
-    //$xPosPlayer = $nbEmptyBox + $xLastPlayerControled;
 
     //------ TODO voir comment on recup cette infos piste session
     $idPlayer = 1;
 
     //Etape 2
-    if(isset($xLimitWorldMap)/* && ($xPosPlayer <= $xLimitWorldMap*/){
+    if(isset($xLimitWorldMap)){
         WorldMap::insertPlayerToWorldMap($idPlayer);
     }
     elseif(!isset($xLimitWorldMap)){
@@ -56,71 +51,14 @@ var_dump("creation de carte");
         /* --- Création de carte --- */
         WorldMap::createWorldMap();
 
-
         /* --- Debut  insertion joueur --- */
-
-        //$idPlayer = 1 ;//recuperé dans les variable de session TODO: à mettre a jour
         WorldMap::insertPlayerToWorldMap($idPlayer);
 
-
-        /* --- Fin insertion joueur --- */
     }
     elseif( $xPosPlayer  > $xLimitWorldMap){
-//TODO a revoir;
-        $yLastPlyLastPlayerControled += 1;
-        ///si yLastPlayerControled >$ylimit alors j agrandis la map
-        //si xpos> xlimit et ypos == y limit
-        // je remet y a 0 et je  
-        WorldMap::updateWorldMapInfos($yLastPlayerControled, Y_LAST_PLAYER);
-        //et x revient a l origine x_depart
-    
-
-        $xPosPlayerUpdated = $xLimitWorldMap - $xLastPlayerControled - $nbEmptyBox;
-        WorldMap::insertPlayerToWorldMap($idPlayer, $xPosPlayerUpdated, $yPosPlayer);
-        WorldMap::updateWorldMapInfos($xPosPlayerUpdated, X_LAST_PLAYER);
+            //TODO recevra une partie de la refactorisqtion de la fonction insertPlayerToWorldMap()
     }
     else{
         //a transformer en log
         echo 'Pb logique d\'implantation';
     }
-
-
-
-
-    /******* controleur de creation de carte  *******/
-
-    /*public static function insertPlayerToWorldMap(){
-
-        $nbEmptyBox = rand(1,4);
-
-        // je recupere la position en x du dernier inscrit
-        $xLastPlayer = WorldMap::getInfoWorldMap(X_LAST_PLAYER);
-
-        // je recupere la position de la limite de carte
-        $xLimitWorldMap = WorldMap::getInfoWorldMap(X_MAP);
-
-        /*
-            SI $xLastPlayer n'est pas definis
-            aucun joueur n'existe donc la carte n'existe pas non plus
-            J'instancie la carte
-        */
-       /* if(!isset($xLastPlayer)){
-            //J'instancie la carte
-            WorldMap::enlargeWorldMap();
-        }
-
-         
-        // si j'essai d'inserer mon joueur en dehors de la carte
-        if($xNewPlayer > $xLimitWorldMap){
-            //j'ajoute une ligne à la carte 
-            // de que sur l'axe des y je suis = à l axe des x 
-            // je agrandis l axe des 5 de x_base
-        }
-        else{
-            //J'insere mon joueur sur la carte
-            //puisque c'est lepremier joueur je serai forcement sur la carte
-            $xNewPlayer = $xLastPlayer + $nbEmptyBox ;//TODO trouver comment definir x 
-
-
-        }
-    }*/
