ade9115cf659a7056bd42d9340f3f6258d71fc19
SebastienHus <hus.sebastien@gmail.com>
2022-12-12 01:28:40 +0100

    remplissage carte selon position differente non fonctionelle

--- testbatiment/vue/component/section/carte.php
+++ testbatiment/vue/component/section/carte.php
@@ -11,17 +11,21 @@
 				<?php
 //TODO a exporter dans une fonction si possible 
 // id_colonie recuperer dans une variable de session
-$xColo = 3;
-$yColo = 4;
+$xColo = 5;
+$yColo = 6;
 
+$xLimitWorldMap = WorldMap::getInfoWorldMap(X_MAX_MAP);
 $xArrayIntervals = WorldMap::determineViewInterval($xColo);
 $xIntervalMin = $xArrayIntervals['intervalMin'];
+$xIntervalMax = $xArrayIntervals['intervalMax'];
+var_dump($xArrayIntervals);
 
-
+$yLimitWorldMap = WorldMap::getInfoWorldMap(Y_MAX_MAP);
 $yArrayIntervals = WorldMap::determineViewInterval($yColo);
 $yIntervalMin = $yArrayIntervals['intervalMin'];
-
-
+$yIntervalMax = $yArrayIntervals['intervalMax'];
+var_dump($yArrayIntervals);
+var_dump($xLimitWorldMap);
 /*
 
 $yInterval = (Y_VUE - 1) / 2;
@@ -33,8 +37,20 @@ $yIntervalMax = $yColo + $yInterval;*/
 
 					//Nombre de lignes
 					for ($y=0; $y < $nbLigne ; $y++) { 
+						$yPos = null;
+						$yMin = $yIntervalMin;
+						if($yMin > $yIntervalMax ){
+							var_dump('if y 1');
+							if($yIntervalMin > $yLimitWorldMap){
+								$yPos = 0;
+							}else {
+								$yPos = $yIntervalMin ;//+ $y;
+						}
+							$yIntervalMin++;
+						}
+						
 
-						$yPos = $yIntervalMin + $y ;
+						//;
 
 						if($y % 2 > 0){
 							?>
@@ -49,9 +65,21 @@ $yIntervalMax = $yColo + $yInterval;*/
 				
 						// nombre de tuile par ligne
 						for ($x=0; $x < $nbTilePerLigne ; $x++) { 
+							$xPos = null;
+							$xMin = $xIntervalMin;
+							if($xIntervalMin > $xIntervalMax ){
+								if($xIntervalMin > $xLimitWorldMap){
+									$xPos = 0;
+								}else {
+								$xPos = $xIntervalMin;// + $x;
+							}
+								$xIntervalMin++;
+							}
+							
 
-							$xPos = $xIntervalMin + $x;
-
+							//
+							var_dump('$xpos : ' . $xPos . ' ypos ' . $yPos);
+//var_dump($xPos);
 							//requete pour recup le biome et l inserer en tan que classe
 							$bdd = Bdd::getBdd();
 							//todo revoir la requete + nom requete ect 
@@ -67,9 +95,9 @@ $yIntervalMax = $yColo + $yInterval;*/
 							$classBiome = $reqGetTile->fetch();
 
 							$reqGetTile->closeCursor();
- 
+							var_dump('$biome : ' . $classBiome[0])
 							?>
-								<div class="hexagon <?= $classBiome[0] ?>"></div>
+								<a href=""><div class="hexagon <?= $classBiome[0] ?>"></div></a>
 							<?php
 
 							//$xIntervalMin++;
